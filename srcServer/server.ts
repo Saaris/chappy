6a7652910be7bc745f296f5605134b313246afa1
Sara Serti <sara.serti@outlook.com>
2025-10-23 14:19:19 +0200

    route files

--- srcServer/server.ts
+++ srcServer/server.ts
@@ -5,3 +5,26 @@ const port: number = Number(process.env.PORT) || 1337
 const app: Express = express()
 
 app.use(express.static('./dist/'))
+
+
+//router modules
+app.use('/api/users')
+
+app.use('/api/channels')
+
+app.use('/api/dm')
+
+//check that the server is started
+app.get('/api/ping', (req: Request, res: Response) => {
+	
+	res.send({ message: 'Pong' })
+})
+
+
+app.listen(port, (error) => {
+	if( error ) {
+		console.log('Server could not start! ', error.message)
+	} else {
+		console.log(`Server is listening on port ${port}...`)
+	}
+})
